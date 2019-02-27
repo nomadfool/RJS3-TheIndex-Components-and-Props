1110f016f3b96632981753dd231c978fdbf27500
NomadFool <nomad.fool@gmail.com>
2019-02-27 15:47:08 +0300

    solution

---
+++ src/AuthorList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+//Components
+import AuthorCard from "./AuthorCard";
+
+class AuthorList extends Component {
+  render() {
+    let author = this.props.authors.map(author => (
+      <AuthorCard author={author} />
+    ));
+    return (
+      <div className="authors">
+        <h3>Authors</h3>
+        <div className="row">{author}</div>
+      </div>
+    );
+  }
+}
+export default AuthorList;
